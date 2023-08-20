fff78555565da6e9e2bef195b22c76d6661da252
codewithrelax <138682266+codewithrelax@users.noreply.github.com>
2023-08-20 14:32:16 +0530

    Update index.php

--- week2/index.php
+++ week2/index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Alan Anand Dsilva</title>
+    <title name="6026815a">Alan Anand Dsilva</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
